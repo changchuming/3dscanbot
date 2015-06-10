4d921660d329ad696f544dd71effc5c8a2a2e753
changchuming <changchuming@gmail.com>
2015-06-10 18:05:02 +0800

    Added meshlab to pipeline

--- osm-bundler/linux/RunBundlerPMVSMeshlab.py
+++ osm-bundler/linux/RunBundlerPMVSMeshlab.py
@@ -1,12 +1,13 @@
 import logging
 import osmbundler
 import osmpmvs
+import subprocess
+import string
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 # initialize OsmBundler manager class
 manager = osmbundler.OsmBundler()
-print manager.workDir
 
 manager.preparePhotos()
 
@@ -26,3 +27,8 @@ managerpmvs.doBundle2PMVS()
 
 # call PMVS
 managerpmvs.doPMVS()
+
+# do meshlab reconstruction
+filename=manager.workDir.split('/')
+print filename[2]
+subprocess.Popen("meshlabserver -i %s/pmvs/models/pmvs_options.txt.ply -o /home/ubuntu/3dscanbot/meshlab/output/%s.x3d -s /home/ubuntu/3dscanbot/meshlab/reconstruction.mlx" % (manager.workDir, filename[2]), shell=True)
