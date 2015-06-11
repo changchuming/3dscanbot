43ce24b07c622f23972ff7050151227703d16a74
changchuming <changchuming@gmail.com>
2015-06-11 22:17:39 +0800

    Added 3dify.me project

--- osm-bundler/linux/RunBundlerPMVSMeshlab.py
+++ osm-bundler/linux/RunBundlerPMVSMeshlab.py
@@ -31,4 +31,5 @@ managerpmvs.doPMVS()
 # do meshlab reconstruction
 filename=manager.workDir.split('/')
 print filename[2]
-subprocess.Popen("meshlabserver -i %s/pmvs/models/pmvs_options.txt.ply -o /home/ubuntu/3dscanbot/meshlab/output/%s.x3d -s /home/ubuntu/3dscanbot/meshlab/reconstruction.mlx" % (manager.workDir, filename[2]), shell=True)
+#subprocess.Popen("meshlabserver -i %s/pmvs/models/pmvs_options.txt.ply -o /home/ubuntu/3dscanbot/meshlab/output/%s.x3d -s /home/ubuntu/3dscanbot/meshlab/reconstruction.mlx" % (manager.workDir, filename[2]), shell=True)
+subprocess.Popen("meshlabserver -i %s/pmvs/models/pmvs_options.txt.ply -o %s/pmvs/models/pmvs_options.txt2.x3d -s /home/ubuntu/3dscanbot/meshlab/reconstruction.mlx" % (manager.workDir, filename[2]), shell=True)
