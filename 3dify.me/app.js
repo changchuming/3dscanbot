893233e95c756dbbbeeb8e955c90af07f2f85b07
Lee Kai Yi <kaiyisg@yahoo.com.sg>
2015-06-12 12:04:38 +0800

    Added button to index.jade, no functionality yet

--- 3dify.me/app.js
+++ 3dify.me/app.js
@@ -20,6 +20,7 @@ app.configure(function(){
 	});
 var http = require('http');
 var path = require('path');
+var formidable = require('formidable');
 
 //----------------------------------------------------------------------------------------------
 // Routes
@@ -80,6 +81,35 @@ jobs.process('queue', function (job, done){
 	})
 	
 setInterval(newJob, 3000);
+
+
+//----------------------------------------------------------------------------------------------
+//Create file upload form
+//----------------------------------------------------------------------------------------------
+http.createServer(function(req, res) {
+	  /* Process the form uploads */
+	  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+	    var form = new formidable.IncomingForm();
+	    form.parse(req, function(err, fields, files) {
+	      res.writeHead(200, {'content-type': 'text/plain'});
+	      res.write('received upload:\n\n');
+	      res.end(util.inspect({fields: fields, files: files}));
+	    });
+	 
+	    return;
+	  }
+	 
+	  /* Display the file upload form. */
+	  res.writeHead(200, {'content-type': 'text/html'});
+	  res.end(
+	    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+	    '<input type="text" name="title"><br>'+
+	    '<input type="file" name="upload" multiple="multiple"><br>'+
+	    '<input type="submit" value="Upload">'+
+	    '</form>'
+	  );
+	 
+	}).listen(8080);
 
 //##############################################################################################
 // Display landing page
