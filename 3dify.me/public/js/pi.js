7728092ccf8a350f93a6c7c475c12271f12f2aa0
changchuming <changchuming@gmail.com>
2015-08-01 20:46:35 +0900

    Still fixing redirection error

--- 3dify.me/public/js/pi.js
+++ 3dify.me/public/js/pi.js
@@ -118,8 +118,7 @@ function piVM(data) {
     }
     
     this.setJobID = function(jobid){
-    	var siteURL = $(location).attr('href').substring(0, $(location).attr('href').lastIndexOf("/"));
-		window.location.href = siteURL+jobid;
+		window.location.href = jobid;
     }
     
     this.newJob = function() {
