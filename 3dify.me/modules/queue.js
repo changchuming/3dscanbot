fded8ce9085db445cc9697a660e04682a9f79585
changchuming <changchuming@gmail.com>
2015-08-19 17:13:25 +0800

    Revert "Fixing issue of error message not showing on too few images"
    This reverts commit a4a17f8ecd5d1ddb0e6bf9ab86add659cce3713f.

--- 3dify.me/modules/queue.js
+++ 3dify.me/modules/queue.js
@@ -13,7 +13,16 @@ exports.initialize = function () {
 // Process queue
 exports.process = function() {
 	jobs.process('reconstruct', function (job, done){
-
+		// Tells server and client which job is current processing
+		// Set current progress to 0 for server and client
+		app.io.room('resultwatch').broadcast('currentprogress', 1);
+		redisClient.set('currentprogress', 1, function(err,reply) {
+			console.log('currentprogress is ' + 1);
+		})
+		app.io.room('resultwatch').broadcast('currentjob', job.id);
+		redisClient.set('currentjob', job.id, function(err,reply) {
+			console.log('Job ' + job.id + ': ' + job.data.iid + ' is now processing');
+		})
 		// Start python reconstruction
 		pyfunc.reconstruct(job.data.iid, done);
 	})
